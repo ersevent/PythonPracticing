5390a25ec96d08a19fc6c0af5d2d95b8ac27c753
Békési Andor <ersevent@gmail.com>
2021-04-18 19:53:31 +0200

    filetask selector

--- modules/fileModule.py
+++ modules/fileModule.py
@@ -4,5 +4,28 @@ def fileWriteExample():
     print("\nWriting to file was successful!")
     file.close()
 
+def fileReadExample():
+    file = open("files/example.txt", "r")
+    print(file.readlines())
+    file.close()
+
+def commands():
+    print("-read")
+    print("-write")
+    print("-exit")
+
 def main():
-    fileWriteExample()
+    while True:
+        print("\nWhat to do with files (type help for commands): ", end=" ")
+        task=input()
+
+        if(task=="write"):
+            fileWriteExample()
+        elif(task=="read"):
+            fileReadExample()
+        elif(task=="help"):
+            commands()
+        elif(task=="exit"):
+            return
+        else:
+            print("Wrong task!")
