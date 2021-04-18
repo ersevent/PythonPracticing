73f762e77568d654086d93400acd8aaa8484ab47
Békési Andor <ersevent@gmail.com>
2021-04-18 19:09:09 +0200

    module selector fix

--- main.py
+++ main.py
@@ -3,32 +3,39 @@ from modules.listModule import listModule
 from modules.basicModule import basicModule
 from modules.functionModule import main as mainFunctionModule
 from modules.exceptionModule import main as mainExceptionModule
+import sys
 
+def commandHelp():
+    print("\n1: Basic module")
+    print("2: List module")
+    print("3: Cycle module")
+    print("4: Function module")
+    print("5: Exception module")
+    print("0: Exit")
 
-print("\n1: Basic module")
-print("2: List module")
-print("3: Cycle module")
-print("4: Function module")
-print("5: Exception module")
-print("\nSelect a module with number: ", end=" ")
-
-try:
-    moduleNum = int(input())
-
-    if(moduleNum==1):
-        basicModule()
-    elif(moduleNum==2):
-        listModule()
-    elif(moduleNum==3):
-        cycleModule()
-    elif(moduleNum==4):
-        mainFunctionModule()
-    elif(moduleNum==5):
-        mainExceptionModule()
-    else: print("\nWrong module number!")
-except ValueError:
-    print("\nInvalid input!")
-
+while True:
+    print("\n--------------------------------------------------------")
+    print("Select a module with number (type help for commands): ", end=" ")
+    
+    try:
+        module = input()
+        if(module=="help"): 
+            commandHelp()
+        elif(int(module)==0):
+            sys.exit()
+        elif(int(module)==1):
+            basicModule()
+        elif(int(module)==2):
+            listModule()
+        elif(int(module)==3):
+            cycleModule()
+        elif(int(module)==4):
+            mainFunctionModule()
+        elif(int(module)==5):
+            mainExceptionModule()
+        else: print("\nWrong module number!")
+    except ValueError:
+        print("Invalid input!")
 
 
 
